ce5a05d8bb36b4da06f07d6f7a4867e4053bffe6
andrey <a.gritsik@gmail.com>
2016-10-28 00:08:05 +0300

    Add search request to omdb

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
 "use strict";
 
 const express = require('express');
+const request = require('request');
+
+const OMDB_URL = 'http://www.omdbapi.com/?s=';
+
 let router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('hi there');
+router.get('/', (req, res) => {
+    res.send('hi there');
+});
+
+router.get('/search', (req, res) => {
+
+    request(OMDB_URL + req.query.s, (err, response, body) => {
+        res.send(body);
+    });
+
 });
 
 module.exports = router;
