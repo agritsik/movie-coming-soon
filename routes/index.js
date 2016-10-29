2cf31f58c8287dce150478d40e4b12a7f08604b2
andrey <a.gritsik@gmail.com>
2016-10-29 23:11:44 +0300

    add reminder end-point and mongodb

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,33 @@
 const express = require('express');
 const request = require('request');
 const redis = require('redis');
+const MongoClient = require('mongodb').MongoClient;
 
 const OMDB_URL = 'http://www.omdbapi.com/'; // todo: settings
 
 // todo: modules http://stackoverflow.com/a/10328308/5253591
 let router = express.Router();
-let redisClient = redis.createClient(6379, '192.168.99.100');
 
+let redisClient = redis.createClient(6379, '192.168.99.100');
 redisClient.on("error", function (err) {
     console.log("Error " + err);
 });
 
+let mongo;
+MongoClient.connect('mongodb://192.168.99.100:27017/csm', function (err, db) {
+    console.log("Error " + err);
+    mongo = db;
+});
+
 router.get('/', (req, res) => {
-    res.send('hi there');
+    res.send('<h1>Coming soon movies API</h1>');
 });
 
 router.get('/search', (req, res) => {
 
     // todo: switch to promises
-    _getData(req.query.s, 2016, (result16) => {
-        _getData(req.query.s, 2017, (result17) => {
+    _search(req.query.s, 2016, (result16) => {
+        _search(req.query.s, 2017, (result17) => {
             let arr1 = JSON.parse(result16).Search;
             let arr2 = JSON.parse(result17).Search;
             res.send(arr1.concat(arr2)); // todo: check if arr is null
@@ -30,8 +37,28 @@ router.get('/search', (req, res) => {
     });
 });
 
+router.post('/reminders', (req, res) => {
+
+    // todo: switch to promises
+    _detail(req.body.imdbID, (data)=> {
+        let reminder = {email: req.body.email, movie: data};
+        mongo.collection('movies').insertOne(reminder, (err, r)=> {
+            res.status(201);
+            res.send();
+        });
+    });
+
+});
+
 // todo: function declaration http://stackoverflow.com/a/336868/5253591
-function _getData(title, year, cb) {
+
+function _detail(id, cb) {
+    request(`${OMDB_URL}?i=${id}&plot=full&r=json`, (err, response, body) => {
+        cb(body);
+    });
+}
+
+function _search(title, year, cb) {
     redisClient.get(`q:${year}:${title}`, (err, reply) => {
         if (reply != null) {
             cb(reply);
