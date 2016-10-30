074ad757063d236c97657429f5484872f4373355
andrey <a.gritsik@gmail.com>
2016-10-30 16:31:48 +0200

    promisify methods

--- routes/index.js
+++ routes/index.js
@@ -27,48 +27,63 @@ router.get('/', (req, res) => {
 
 router.get('/search', (req, res) => {
 
-    // todo: switch to promises
-    _search(req.query.s, 2016, (result16) => {
-        _search(req.query.s, 2017, (result17) => {
-            let arr1 = JSON.parse(result16).Search;
-            let arr2 = JSON.parse(result17).Search;
-            res.send(arr1.concat(arr2)); // todo: check if arr is null
+    // thanks to: http://exploringjs.com/es6/ch_promises.html#_map-via-promiseall
+    const years = [2016, 2017];
+    Promise
+        .all(years.map(e => _search(req.query.s, e)))
+        .then(r => {
+            const map = r.map(e => JSON.parse(e).Search); // todo: check if arr is null
+            res.send([].concat.apply([], map));
         });
-    });
 });
 
 router.post('/reminders', (req, res) => {
 
-    // todo: switch to promises
-    _detail(req.body.imdbID, (data)=> {
-        let reminder = {email: req.body.email, movie: data};
-        mongo.collection('movies').insertOne(reminder, (err, r)=> {
-            res.status(201);
-            res.send();
-        });
+    _detail(req.body.imdbID).then(data => {
+        mongo.collection('movies')
+            .insertOne({email: req.body.email, movie: data})
+            .then(r => res.status(201).send());
     });
 
 });
 
 // todo: function declaration http://stackoverflow.com/a/336868/5253591
-
-function _detail(id, cb) {
-    request(`${OMDB_URL}?i=${id}&plot=full&r=json`, (err, response, body) => {
-        cb(body);
+/**
+ * Loads movie details by id
+ * @param id
+ * @returns {Promise}
+ * @private
+ */
+function _detail(id) {
+    return new Promise((resolve, reject) => {
+        request(`${OMDB_URL}?i=${id}&plot=full&r=json`, (err, response, body) => {
+            resolve(body);
+        });
     });
 }
 
-function _search(title, year, cb) {
-    redisClient.get(`q:${year}:${title}`, (err, reply) => {
-        if (reply != null) {
-            cb(reply);
-        } else {
-            request(`${OMDB_URL}?s=${title}&y=${year}&type=movie`, (err, response, body) => {
-                redisClient.setex(`q:${year}:${title}`, 3600, body);
-                cb(body);
-            });
-        }
-    })
+
+/**
+ * Loads movies by title and year
+ * @param title
+ * @param year
+ * @returns {Promise}
+ * @private
+ */
+function _search(title, year) {
+    return new Promise((resolve, reject)=> {
+        redisClient.get(`q:${year}:${title}`, (err, reply) => {
+            if (reply != null) {
+                resolve(reply);
+            } else {
+                request(`${OMDB_URL}?s=${title}&y=${year}&type=movie`, (err, response, body) => {
+                    redisClient.setex(`q:${year}:${title}`, 3600, body);
+                    resolve(body);
+                });
+            }
+        })
+    });
+
 }
 
 module.exports = router;
